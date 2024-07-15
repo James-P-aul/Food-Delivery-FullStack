a9843924ddeaef94f796bcb32ebe5c4baf9bb67a
James-P-aul <119072034+James-P-aul@users.noreply.github.com>
2024-07-15 20:43:42 +0530

    Update App.jsx

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const url = "http://localhost:8000";
+  const url = "https://food-del-backend-ggjj.onrender.com";
   return (
     <div>
       <ToastContainer />
